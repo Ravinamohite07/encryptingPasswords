fe75a7c529d213af389592a09c955760a6a549d5
Ravinamohite07 <ravinamohite527@gmail.com>
2025-03-03 15:34:25 +0530

    adding login api

--- src/app.js
+++ src/app.js
@@ -34,6 +34,27 @@ app.post("/signup",async (req,res) =>{
      }; 
 }); 
 
+app.post("/login", async(req,res) =>{
+    try{
+        const {emailId, password} = req.body;
+
+        const user = await User.findOne({emailId: emailId});
+        if(!user){
+            throw new Error("Invalid credentials");
+        }
+        const isPasswordValid =  await bcrypt.compare(password,user.password);
+        if(isPasswordValid){
+            res.send("user successfully login...!");
+        } else{
+            throw new Error("password not correct..!");
+        }
+
+    } catch(err){
+        res.status(400).send("Error:" + err.message);
+     }; 
+}); 
+
+
 app.delete("/user",async (req,res) => {
     const userId = req.body.userId;
     try{
